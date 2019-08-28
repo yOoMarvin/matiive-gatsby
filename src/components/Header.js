4687cf5c525468b92bd238de11a267db7f9bc74a
yoomarvin <marvin.messenzehl@gmail.com>
2019-08-28 20:01:12 +0200

    making the header a bit more... real 🤷‍♂️

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,54 @@
 import React from "react"
 import styled from "styled-components"
-import { redirectTo } from "@reach/router"
 
-const Title = styled.h1`
-  color: red;
-  padding: 8px;
+const MyHeader = styled.header`
+  z-index: 5;
+  padding-top: 16px;
+`
+const Container = styled.div`
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-left: auto;
+  margin-right: auto;
+`
+const Left = styled.div`
+  float: left;
+`
+const Title = styled.a`
+  margin 0;
+  line-hight: 16px;
+  text-decoration: none;
+  font-size: 1.5em;
+  font-weight: bold;
+  color: #3C414B;
+`
+const Right = styled.div`
+  float: right;
+  padding-top: 8px;
+  margin-right: 10%;
+`
+const HeaderLink = styled.a`
+  padding-right: 15px;
+  padding-left: 15px;
+  text-decoration: none;
 `
 
 class Header extends React.Component {
   render() {
     return (
-      <div>
-        <Title>matiive insurance</Title>
-      </div>
+      <MyHeader>
+        <Container>
+          <Left>
+            <Title href="/">matiive</Title>
+          </Left>
+          <Right>
+            <HeaderLink href="/liability">Liability</HeaderLink>
+            <HeaderLink href="/contents">Contents</HeaderLink>
+            <HeaderLink href="/bike">Bike</HeaderLink>
+            <HeaderLink href="/cyber">Cyber</HeaderLink>
+          </Right>
+        </Container>
+      </MyHeader>
     )
   }
 }
