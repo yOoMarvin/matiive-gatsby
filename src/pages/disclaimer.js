0680818432c4fecb28560b9253dd087f85d17747
yoomarvin <marvin.messenzehl@gmail.com>
2019-09-08 17:39:48 +0200

    disclaimer page

---
+++ src/pages/disclaimer.js
@@ -0,0 +1,290 @@
+import React from "react"
+import styled from "styled-components"
+
+import Header from "../components/Header"
+
+const Container = styled.div`
+  padding: 15px;
+`
+class Contents extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container>
+          <h1>Disclaimer</h1>
+          <p>
+            maative ist ein fiktiver Versicherer welcher zu Experimentszwecken
+            an der Uni Mannheim entwickelt wurde. Die dargestellten Produkte
+            sind nicht real und es besteht keinerlei Anspruch auf Leistung. Mit
+            der Nutzung der Website willigen Sie der Datenschutzerklärung sowie
+            der Verwendung von Cookies ein. Diese werden auf der Webseite
+            verwendet um die User Experience zu steigern.
+          </p>
+          <h1>Impressum</h1>
+          <p>
+            <b>Verantwortlicher nach § 5 Telemediengesetz (TMG)</b>
+          </p>
+          <p>
+            marvinmessenzehl.com <br />
+            Marvin Messenzehl
+            <br />
+            Kalthorststraße 29
+            <br />
+            68307 Mannheim
+          </p>
+          <p>hello@marvinmessenzehl.com</p>
+          <br />
+
+          <h2>Rechtliche Hinweise</h2>
+          <h3>§ 1 Haftungsbeschränkung</h3>
+          <p>
+            Die Inhalte dieser Website werden mit größtmöglicher Sorgfalt
+            erstellt. Der Anbieter übernimmt jedoch keine Gewähr für die
+            Richtigkeit, Vollständigkeit und Aktualität der bereitgestellten
+            Inhalte. Die Nutzung der Inhalte der Website erfolgt auf eigene
+            Gefahr des Nutzers. Namentlich gekennzeichnete Beiträge geben die
+            Meinung des jeweiligen Autors und nicht immer die Meinung des
+            Anbieters wieder. Mit der reinen Nutzung der Website des Anbieters
+            kommt keinerlei Vertragsverhältnis zwischen dem Nutzer und dem
+            Anbieter zustande.
+          </p>
+
+          <h3>§ 2 Externe Links</h3>
+          <p>
+            Diese Website enthält Verknüpfungen zu Websites Dritter („externe
+            Links“). Diese Websites unterliegen der Haftung der jeweiligen
+            Betreiber. Der Anbieter hat bei der erstmaligen Verknüpfung der
+            externen Links die fremden Inhalte daraufhin überprüft, ob etwaige
+            Rechtsverstöße bestehen. Zu dem Zeitpunkt waren keine Rechtsverstöße
+            ersichtlich. Der Anbieter hat keinerlei Einfluss auf die aktuelle
+            und zukünftige Gestaltung und auf die Inhalte der verknüpften
+            Seiten. Das Setzen von externen Links bedeutet nicht, dass sich der
+            Anbieter die hinter dem Verweis oder Link liegenden Inhalte zu Eigen
+            macht. Eine ständige Kontrolle der externen Links ist für den
+            Anbieter ohne konkrete Hinweise auf Rechtsverstöße nicht zumutbar.
+            Bei Kenntnis von Rechtsverstößen werden jedoch derartige externe
+            Links unverzüglich gelöscht.
+          </p>
+
+          <h3>§ 3 Urheber- und Leistungsschutzrechte</h3>
+          <p>
+            Die auf dieser Website veröffentlichten Inhalte unterliegen dem
+            deutschen Urheber- und Leistungsschutzrecht. Jede vom deutschen
+            Urheber- und Leistungsschutzrecht nicht zugelassene Verwertung
+            bedarf der vorherigen schriftlichen Zustimmung des Anbieters oder
+            jeweiligen Rechteinhabers. Dies gilt insbesondere für
+            Vervielfältigung, Bearbeitung, Übersetzung, Einspeicherung,
+            Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken oder
+            anderen elektronischen Medien und Systemen. Inhalte und Rechte
+            Dritter sind dabei als solche gekennzeichnet. Die unerlaubte
+            Vervielfältigung oder Weitergabe einzelner Inhalte oder kompletter
+            Seiten ist nicht gestattet und strafbar. Lediglich die Herstellung
+            von Kopien und Downloads für den persönlichen, privaten und nicht
+            kommerziellen Gebrauch ist erlaubt.
+          </p>
+          <p>
+            Die Darstellung dieser Website in fremden Frames ist nur mit
+            schriftlicher Erlaubnis zulässig.
+          </p>
+
+          <h3>§ 4 Besondere Nutzungsbedingungen</h3>
+          <p>
+            Soweit besondere Bedingungen für einzelne Nutzungen dieser Website
+            von den vorgenannten Paragraphen abweichen, wird an entsprechender
+            Stelle ausdrücklich darauf hingewiesen. In diesem Falle gelten im
+            jeweiligen Einzelfall die besonderen Nutzungsbedingungen.
+          </p>
+          <h1>Datenschutzerklärung</h1>
+          <p>
+            Nachfolgend möchten wir Sie über unsere Datenschutzerklärung
+            informieren. Sie finden hier Informationen über die Erhebung und
+            Verwendung persönlicher Daten bei der Nutzung unserer Webseite. Wir
+            beachten dabei das für Deutschland geltende Datenschutzrecht. Sie
+            können diese Erklärung jederzeit auf unserer Webseite abrufen.
+          </p>
+          <p>
+            Wir weisen ausdrücklich darauf hin, dass die Datenübertragung im
+            Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken
+            aufweisen und nicht lückenlos vor dem Zugriff durch Dritte geschützt
+            werden kann.
+          </p>
+          <p>
+            Die Verwendung der Kontaktdaten unseres Impressums zur gewerblichen
+            Werbung ist ausdrücklich nicht erwünscht, es sei denn wir hatten
+            zuvor unsere schriftliche Einwilligung erteilt oder es besteht
+            bereits eine Geschäftsbeziehung. Der Anbieter und alle auf dieser
+            Website genannten Personen widersprechen hiermit jeder kommerziellen
+            Verwendung und Weitergabe ihrer Daten.
+          </p>
+
+          <h3>Personenbezogene Daten</h3>
+          <p>
+            Sie können unsere Webseite ohne Angabe personenbezogener Daten
+            besuchen. Soweit auf unseren Seiten personenbezogene Daten (wie
+            Name, Anschrift oder E-Mail Adresse) erhoben werden, erfolgt dies,
+            soweit möglich, auf freiwilliger Basis. Diese Daten werden ohne Ihre
+            ausdrückliche Zustimmung nicht an Dritte weitergegeben. Sofern
+            zwischen Ihnen und uns ein Vertragsverhältnis begründet, inhaltlich
+            ausgestaltet oder geändert werden soll oder Sie an uns eine Anfrage
+            stellen, erheben und verwenden wir personenbezogene Daten von Ihnen,
+            soweit dies zu diesen Zwecken erforderlich ist (Bestandsdaten). Wir
+            erheben, verarbeiten und nutzen personenbezogene Daten soweit dies
+            erforderlich ist, um Ihnen die Inanspruchnahme des Webangebots zu
+            ermöglichen (Nutzungsdaten). Sämtliche personenbezogenen Daten
+            werden nur solange gespeichert wie dies für den geannten Zweck
+            (Bearbeitung Ihrer Anfrage oder Abwicklung eines Vertrags)
+            erforderlich ist. Hierbei werden steuer- und handelsrechtliche
+            Aufbewahrungsfristen berücksichtigt. <br />
+            Auf Anordnung der zuständigen Stellen dürfen wir im Einzelfall
+            Auskunft über diese Daten (Bestandsdaten) erteilen, soweit dies für
+            Zwecke der Strafverfolgung, zur Gefahrenabwehr, zur Erfüllung der
+            gesetzlichen Aufgaben der Verfassungsschutzbehörden oder des
+            Militärischen Abschirmdienstes oder zur Durchsetzung der Rechte am
+            geistigen Eigentum erforderlich ist.
+          </p>
+
+          <h3>
+            Datenschutzerklärung für den Webanalysedienst Google Analytics
+          </h3>
+          <p>
+            Auf dieser Website wird Google Analytics eingesetzt, ein
+            Webanalysedienst der Google Inc. (&bdquo;Google&ldquo;). Die Nutzung
+            umfasst die Betriebsart &bdquo;Universal Analytics&ldquo;. Hierdurch
+            ist es möglich, Daten, Sitzungen und Interaktionen über mehrere
+            Geräte hinweg einer pseudonymen User-ID zuzuordnen und so die
+            Aktivitäten eines Nutzers geräteübergreifend zu analysieren. Dieser
+            Datenschutzhinweis wird zur Verfügung gestellt von{" "}
+            <a href="https://www.intersoft-consulting.de" target="_blank">
+              www.intersoft-consulting.de
+            </a>
+            .
+          </p>
+
+          <p>
+            Google Analytics verwendet sog. &bdquo;Cookies&ldquo;, Textdateien,
+            die auf Ihrem Computer gespeichert werden und die eine Analyse der
+            Benutzung der Website durch Sie ermöglichen. Die durch das Cookie
+            erzeugten Informationen über Ihre Benutzung dieser Website werden in
+            der Regel an einen Server von Google in den USA übertragen und dort
+            gespeichert. Im Falle der Aktivierung der IP-Anonymisierung auf
+            dieser Website, wird Ihre IP-Adresse von Google jedoch innerhalb von
+            Mitgliedstaaten der Europäischen Union oder in anderen
+            Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum
+            zuvor gekürzt. Nur in Ausnahmefällen wird die volle IP-Adresse an
+            einen Server von Google in den USA übertragen und dort gekürzt. Wir
+            weisen Sie darauf hin, dass auf dieser Webseite Google Analytics um
+            eine IP-Anonymisierung erweitert wurde, um eine anonymisierte
+            Erfassung von IP-Adressen (sog. IP-Masking) zu gewährleisten. Die im
+            Rahmen von Google Analytics von Ihrem Browser übermittelte
+            IP-Adresse wird nicht mit anderen Daten von Google zusammengeführt.
+            Nähere Informationen zu Nutzungsbedingungen und Datenschutz finden
+            Sie unter{" "}
+            <a
+              href="https://www.google.com/analytics/terms/de.html"
+              target="_blank"
+            >
+              https://www.google.com/analytics/terms/de.html
+            </a>{" "}
+            bzw. unter{" "}
+            <a href="https://policies.google.com/?hl=de" target="_blank">
+              https://policies.google.com/?hl=de
+            </a>
+            .
+          </p>
+
+          <p>
+            <strong>Zwecke der Verarbeitung</strong>
+            <br />
+            Im Auftrag des Betreibers dieser Website wird Google diese
+            Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um
+            Reports über die Websiteaktivitäten zusammenzustellen und um weitere
+            mit der Websitenutzung und der Internetnutzung verbundene
+            Dienstleistungen gegenüber dem Websitebetreiber zu erbringen.
+          </p>
+          <p>
+            <strong>Rechtsgrundlage</strong>
+            <br />
+            Die Rechtsgrundlage für den Einsatz von Google Analytics ist Ihre
+            Einwilligung gemäß{" "}
+            <a href="https://dsgvo-gesetz.de/art-6-dsgvo/" target="_blank">
+              Art. 6 Abs. 1 S.1 lit. a DSGVO
+            </a>
+            .
+          </p>
+          <p>
+            <strong>Empfänger / Kategorien von Empfängern</strong>
+            <br />
+            Der Empfänger der erhobenen Daten ist Google.
+          </p>
+          <p>
+            <strong>Übermittlung in Drittstaaten</strong>
+            <br />
+            Die personenbezogenen Daten werden unter dem EU-US Privacy Shield
+            auf Grundlage des Angemessenheitsbeschlusses der Europäischen
+            Kommission in die USA übermittelt. Das Zertifikat können Sie{" "}
+            <a
+              href="https://www.privacyshield.gov/participant?id=a2zt000000001L5AAI"
+              target="_blank"
+            >
+              hier
+            </a>{" "}
+            abrufen.
+          </p>
+          <p>
+            <strong>Dauer der Datenspeicherung</strong>
+            <br />
+            Die von uns gesendeten und mit Cookies, Nutzerkennungen (z. B.
+            User-ID) oder Werbe-IDs verknüpften Daten werden nach 12 Monaten
+            automatisch gelöscht. Die Löschung von Daten, deren
+            Aufbewahrungsdauer erreicht ist, erfolgt automatisch einmal im
+            Monat.
+          </p>
+          <p>
+            <strong>Betroffenenrechte</strong>
+            <br />
+            Sie können Ihre Einwilligung jederzeit mit Wirkung für die Zukunft
+            widerrufen, indem Sie die Speicherung der Cookies durch eine
+            entsprechende Einstellung Ihrer Browser-Software verhindern; wir
+            weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls
+            nicht sämtliche Funktionen dieser Website vollumfänglich werden
+            nutzen können.
+          </p>
+
+          <p>
+            Sie können darüber hinaus die Erfassung der durch das Cookie
+            erzeugten und auf Ihre Nutzung der Website bezogenen Daten (inkl.
+            Ihrer IP-Adresse) an Google sowie die Verarbeitung dieser Daten
+            durch Google verhindern, indem Sie das{" "}
+            <a
+              href="https://tools.google.com/dlpage/gaoptout?hl=de"
+              target="_blank"
+            >
+              Browser-Add-on
+            </a>{" "}
+            herunterladen und installieren. Opt-Out-Cookies verhindern die
+            zukünftige Erfassung Ihrer Daten beim Besuch dieser Website. Um die
+            Erfassung durch Universal Analytics über verschiedene Geräte hinweg
+            zu verhindern, müssen Sie das Opt-Out auf allen genutzten Systemen
+            durchführen. Wenn Sie hier klicken, wird das Opt-Out-Cookie gesetzt:{" "}
+            <a href="javascript:gaOptout()">
+              <strong>Google Analytics deaktivieren</strong>
+            </a>
+          </p>
+
+          <h3>Auskunftsrecht</h3>
+          <p>
+            Sie haben das jederzeitige Recht, sich unentgeltlich und
+            unverzüglich über die zu Ihrer Person erhobenen Daten zu erkundigen.
+            Sie haben das jederzeitige Recht, Ihre Zustimmung zur Verwendung
+            Ihrer angegeben persönlichen Daten mit Wirkung für die Zukunft zu
+            widerrufen. Zur Auskunftserteilung wenden Sie sich bitte an den
+            Anbieter unter den Kontaktdaten im Impressum.
+          </p>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default Contents
